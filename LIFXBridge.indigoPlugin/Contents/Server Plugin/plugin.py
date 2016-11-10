7cf95c27002bf6a616de1272ddd32161671d80ad
Joe Keenan <joe@flyingdiver.com>
2016-11-10 14:09:08 -0500

    typos

--- LIFXBridge.indigoPlugin/Contents/Server Plugin/plugin.py
+++ LIFXBridge.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -313,10 +313,10 @@ class Plugin(indigo.PluginBase):
         source = message.source_id
         seq_num = message.seq_num
 
-        if seq_num > self.last_seq_num
+        if seq_num > self.last_seq_num:
             self.last_seq_num = seq_num
             self.debugLog("lifxRespond, processing message seq_num = " + str(seq_num))
-        else
+        else:
             self.debugLog("lifxRespond, skipping message, duplicate or out of order seq_num = " + str(seq_num))
             return
 
